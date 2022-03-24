4f85fa75dfb3433b5bf8a152883a37ee1b8e2ee1
KatonaaAdam <78982283+KatonaaAdam@users.noreply.github.com>
2022-03-24 15:45:07 +0100

    make it better

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
 // Importing http module
-const http = require("http")
-  
-// Creating Server
-const server = http.createServer((req,res)=>{
-    req.statusCode=200;
-    console.log("Server is Started")
-    res.end();
-});
+const express = require("express")
+
+const app=express();
+
+const port = process.env.PORT || 3000;  
+
+
+app.get('/',(req,res)=>{
+    res.send('My site!')
+})
   
 // Executing the server
-server.listen(3000,"localhost",()=>{
-    console.log("Server is Running ")
+app.listen(port,()=>{
+    console.log("Server is Running!!! ");
 })
