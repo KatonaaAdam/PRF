f1a951f98835ab654ac0772785660dd761c849f3
KatonaaAdam <78982283+KatonaaAdam@users.noreply.github.com>
2022-04-18 20:48:16 +0200

    location change

--- api/aru.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-var aruSchema= new mongoose.Schema({
-    id:{type: String,unique:true, required:true,lowercase:true},
-    value:{type: String, required:true,}
-}, {collection:'aru'});
-
-mongoose.model('aru',aruSchema);
